bad933d7f3b3dc468476dcfc98430a9deb9517ec
a.appeddu <a.appeddu@appius.it>
2019-03-06 16:54:18 +0100

    commit new files

---
+++ source/android/app/src/main/java/matera/fablab/com/matera2019/FilesListActivity.java
@@ -0,0 +1,74 @@
+package matera.fablab.com.matera2019;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.apache.http.NameValuePair;
+
+import java.util.List;
+
+public class FilesListActivity extends Activity {
+
+    Context _context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.files_list);
+        _context = this;
+
+        final LinearLayout filesListLayout = findViewById(R.id.filesListLayout);
+
+        Thread networkThread = new Thread(){
+
+            @Override
+            public void run() {
+                super.run();
+                String res = HttpCall.loadUrl(_context, "http://www.appius.it/matera/gcode_files", false);
+                String splittedResult[] = res.split("\n");
+
+                for (final String line : splittedResult){
+
+                    Handler mainHandler = new Handler(_context.getMainLooper());
+
+                    Runnable myRunnable = new Runnable() {
+                        @Override
+                        public void run() {
+
+                            String splittedLine[] = line.split("-_-");
+                            String fileName = splittedLine[0];
+                            TextView textView = new TextView(_context);
+                            textView.setText(fileName);
+
+                            filesListLayout.addView(textView);
+
+                            textView.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+
+                                }
+                            });
+
+                        }
+                    };
+                    mainHandler.post(myRunnable);
+
+
+
+                }
+
+                int asd = 34;
+            }
+        };
+
+        networkThread.start();
+
+    }
+
+}
