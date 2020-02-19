7f5cb83198e89709795f8cc6cbc5867d64b7d18d
Simone Perini <39993316+CoffeePerry@users.noreply.github.com>
2020-02-19 22:40:21 +0100

    Ri-allineamento codice 20200219

--- source/php/save.php
+++ source/php/save.php
@@ -4,41 +4,49 @@ function gen_uuid() {
     return sprintf ( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x', mt_rand ( 0, 0xffff ), mt_rand ( 0, 0xffff ), mt_rand ( 0, 0xffff ), mt_rand ( 0, 0x0fff ) | 0x4000, mt_rand ( 0, 0x3fff ) | 0x8000, mt_rand ( 0, 0xffff ), mt_rand ( 0, 0xffff ), mt_rand ( 0, 0xffff ) );
 }
 
+  $lines = $_POST['points'];
+  $title = $_POST['title'];
+  $fileName = $_POST['fileName'];
+  $format = $_POST['format'];
 
-    $lines = $_POST['points'];
-    $title = $_POST['title'];
-    $fileName = $_POST['fileName'];
-
-    $folderPath = 'gcodes/';
+  $folderPath = 'gcodes/';
 	$tracks = explode("$$", $lines);
 
 
   if ($fileName == ''){
     $newFile = true;
-    $name = $title.'-_-'.gen_uuid();
+    $name = $title;
   }
   else {
     $name = $fileName;
     $newFile = false;
   }
 
-	$fileName = $name.".gcode";
-	$fileNameCoord = $name.".coord";
+	$fileName = $name.".".$format;
 
-	$myfileCoord = fopen($folderPath.$fileNameCoord, "w") or die("Unable to open file!");
-	fwrite($myfileCoord, $lines);
+
+  if (strcmp($format, "coord") == 0){
+  	$myfileCoord = fopen($folderPath.$fileName, "w") or die("Unable to open file!");
+  	fwrite($myfileCoord, $lines);
+  	fclose($myfileCoord);
+    exit(0);
+  }
 
 	$myfile = fopen($folderPath.$fileName, "w") or die("Unable to open file!");
 
 	error_log ("filename = ".$fileName);
 
-
   $startCmd = "G21G90\n";
   $cmd = "G0";
   $cmdSpeed = "F10";
 
-
   fwrite($myfile, $startCmd);
+
+  // abilito il relè
+  $command = "M3"."\n";
+  fwrite($myfile, $command);
+  $command = "S0"."\n";
+  fwrite($myfile, $command);
 
 	foreach ($tracks as &$track) {
 
@@ -47,26 +55,39 @@ function gen_uuid() {
 		for ($i = 0; $i < count($points); $i = $i + 2) {
 
 		  error_log ("point ");
-			$x = $points[$i];
-			$y = $points[$i + 1];
+			$x = 85 - ($points[$i] + 5);
+			$y = 85 - ($points[$i + 1] + 5);
 
 			if ($i == 0){
 				$command = $cmd."Z0"."X".$x."Y".$y.$cmdSpeed."\n";
 				fwrite($myfile, $command);
+
+        // accendo la lampada
+        $command = "S1000"."\n";
+        fwrite($myfile, $command);
 			}
 
-      $command = $cmd."Z1"."X".$x."Y".$y.$cmdSpeed."\n";
+      $command = $cmd."Z0"."X".$x."Y".$y.$cmdSpeed."\n";
 			fwrite($myfile, $command);
 
 			if ($i == count($points) - 2){
-        $command = $cmd."Z0"."X".$x."Y".$y.$cmdSpeed."\n";
-				fwrite($myfile, $command);
+
+        // spengo la lampada
+        $command = "S0"."\n";
+        fwrite($myfile, $command);
+
+
 			}
 		}
+
 	}
 
+  // disabilito il relè
+  $command = "M5"."\n";
+  fwrite($myfile, $command);
+
 	fclose($myfile);
-	fclose($myfileCoord);
+
 
   if ($newFile == true){
   	$fileListFile = fopen("gcode_files", "a") or die("Unable to open file!");
